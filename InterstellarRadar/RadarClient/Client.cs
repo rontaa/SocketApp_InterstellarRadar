796fb222398298d0c27eb98ab5e08f8f6189c434
rontaa <pareoroberto9@gmail.com>
2026-05-06 16:47:00 +0200

    Versione 1.0: Aggiunto parte di codice Server e parte di codice Client

--- InterstellarRadar/RadarClient/Client.cs
+++ InterstellarRadar/RadarClient/Client.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System.Net.Sockets;
+using System.Text;
+
+Console.Title = "TERMINALE RADAR"; // Titolo della finestra console
+Console.CursorVisible = false;      // Nasconde il trattino lampeggiante fastidioso
+
+//Ciclo infinito per far andare il gioco
+while (true)
+{
+    // Variabile comando: di base è "PING" (chiede solo dati) ma se premo un tasto cambia
+    string comando = "PING";
+ 
+    // KeyAvailable controlla se ho premuto un tasto senza fermare il radar mentre aspetta.
+    if (Console.KeyAvailable)
+    {
+        var k = Console.ReadKey(true).Key; // Legge il tasto premuto (true fa in modo che non lo scrive a schermo)
+        comando = k.ToString();            // Trasforma il tasto in testo ( per esempio i tasti usati per muoversi " W A S D " )
+    }
+
+
+}
--- InterstellarRadar/RadarServer/Server.cs
+++ InterstellarRadar/RadarServer/Server.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Random rng = new Random(); // Generatore di numeri casuali per creare la mappa ogni volta diversa
+
+int pX = 0, pY = 0; // Coordinate X e Y del giocatore (parte da 0,0)
+
+int energia = 100; // Risorsa limitata, cala muovendosi, se arriva a 0 il gioco finisce
+
+int tanichePrese = 0; // Contatore degli obiettivi raggiunti (punteggio del giocatore)
+
+// Lista per gli asteroidi, rappresentano ostacoli che non vanno via nella mappa (è solo per estetica)
+var asteroidi = new List<(int x, int y)>();
